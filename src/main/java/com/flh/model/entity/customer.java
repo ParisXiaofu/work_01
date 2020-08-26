d6f2cb4b9a4b4749f4c76e72ce1c0d6c440292a6
Zhen <guozhenhero@gmail.com>
2020-08-26 10:07:58 +0800

    feat:this is a exercise

---
+++ src/main/java/com/flh/model/entity/customer.java
@@ -0,0 +1,43 @@
+package com.flh.model.entity;
+
+public class customer {
+    private int custId;
+    private String custTel;
+    private String custName;
+    private String custPwd;
+
+    public customer() {
+    }
+
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    public String getCustTel() {
+        return custTel;
+    }
+
+    public void setCustTel(String custTel) {
+        this.custTel = custTel;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getCustPwd() {
+        return custPwd;
+    }
+
+    public void setCustPwd(String custPwd) {
+        this.custPwd = custPwd;
+    }
+}
